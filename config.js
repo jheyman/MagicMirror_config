0c6727482f1506fd48f0d1ed65c135098d53cbff
jheyman <bidsomail@gmail.com>
2020-04-12 12:46:07 +0200

    first stable config

--- config.js
+++ config.js
@@ -10,26 +10,26 @@
 
 var config = {
 	address: "0.0.0.0",  // Address to listen on, can be:
-	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-	                      // - another specific IPv4/6 to listen on a specific interface
-	                      // - "", "0.0.0.0", "::" to listen on any interface
-	                      // Default, when address config is left out, is "localhost"
+	// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	// - another specific IPv4/6 to listen on a specific interface
+	// - "", "0.0.0.0", "::" to listen on any interface
+	// Default, when address config is left out, is "localhost"
 	port: 8080,
 	ipWhitelist: [],
-//	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.0.0/24"], // Set [] to allow all IP addresses
-	                                                       // or add a specific IPv4 of 192.168.1.5 :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+	//	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.0.0/24"], // Set [] to allow all IP addresses
+	// or add a specific IPv4 of 192.168.1.5 :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
 	language: "fr",
 	timeFormat: 24,
 	units: "metric",
 	// serverOnly:  true/false/"local" ,
-			     // local for armv6l processors, default 
-			     //   starts serveronly and then starts chrome browser
-			     // false, default for all  NON-armv6l devices
-			     // true, force serveronly mode, because you want to.. no UI on this device
+	// local for armv6l processors, default 
+	//   starts serveronly and then starts chrome browser
+	// false, default for all  NON-armv6l devices
+	// true, force serveronly mode, because you want to.. no UI on this device
 
 	modules: [
 		{
@@ -38,48 +38,69 @@ var config = {
 		},
 		{
 			module: "updatenotification",
-			disabled: false,
+			disabled: true,
 			position: "top_bar"
 		},
 		{
-			module: "MMM-Keyboard",
-			disabled: true,
-			position: "top_left",
-			config: {
-			locale: "de-DE"
-			}
+			module: "MMM-ShoppingList",
+			disabled: false,
+			position: "bottom_left"
 		},
+		{
+			module: "MMM-Keyboard",
+			disabled: false,
+			position: "middle_center",
+			config: {
+				locale: "fr-FR",
+				debug: false
+			}
+		},		
 		{
 			module: 'MMM-OnScreenMenu',
 			disabled: false,
-		        position: 'bottom_right',
-		        /* Valid positions: 'top_right', 'top_left', 'bottom_right', 'bottom_left' */
-		        config: {
-		        	touchMode: true,
-		                enableKeyboard: false,
+			position: 'bottom_right',
+			/* Valid positions: 'top_right', 'top_left', 'bottom_right', 'bottom_left' */
+			config: {
+				touchMode: true,
+				enableKeyboard: false,
 				menuItems: {
-					moduleHide1: { title: "Hide Clock", icon: "minus-square", name: "clock" },
-	                                moduleShow1: { title: "Show Clock", icon: "plus-square", name: "clock" },
-					refresh: { title: "Refresh", icon: "recycle", source: "LOCAL" },
-					moduleHide2: { title: "XXXXX", icon: "minus-square", name: "clock" },
-					moduleShow2: { title: "YYYYY", icon: "plus-square", name: "clock" },
-				 //   restart: { title: "Restart MagicMirror", icon: "refresh", source: "ALL" },
+					refresh: { title: "REFRESH", icon: "recycle", source: "LOCAL" },
+					notify1: {
+						title: "SLIDESHOW_NEXT",
+						icon: "forward",
+						notification: "BACKGROUNDSLIDESHOW_NEXT",
+						payload: ""
+					},
+					notify2: {
+						title: "SLIDESHOW_PAUSE",
+						icon: "pause",
+						notification: "BACKGROUNDSLIDESHOW_PAUSE",
+						payload: ""
+					},
+					notify3: {
+						title: "SLIDESHOW_RESUME",
+						icon: "play",
+						notification: "BACKGROUNDSLIDESHOW_PLAY",
+						payload: ""
+					},
 				},
 			}
-	        },
+		},
 		{
 			module: 'MMM-BackgroundSlideshow',
 			disabled: false,
 			position: 'fullscreen_below',
 			config: {
 				imagesTopDirectory: 'modules/MMM-BackgroundSlideshow/exampleImages/link',
+				//imagesTopDirectory: 'modules/MMM-BackgroundSlideshow/exampleImages',
 				excludedImagePaths: ['modules/MMM-BackgroundSlideshow/exampleImages/link/.Trash-1000'],
-				filteredDirectoriesKeyword: 'NO_SHOW_',
-			   	transitionImages: true,
+				filteredDirectoriesKeyword: 'NOSHOW_',
+				transitionImages: true,
 				recursiveSubDirectories: true,
-				slideshowSpeed: 30000, 
-				backgroundSize: "cover",
-				gradient: [ "rgba(0, 0, 0, 0.0) 0%", "rgba(0, 0, 0, 0) 100%" ]
+				//recursiveSubDirectories: false,
+				slideshowSpeed: 30000,
+				backgroundSize: "contain",
+				gradient: ["rgba(0, 0, 0, 0.0) 0%", "rgba(0, 0, 0, 0) 100%"]
 			}
 		},
 		{
@@ -92,7 +113,7 @@ var config = {
 			lon: 2.1888326,
 		},
 		{
-			disabled: false,
+			disabled: true,
 			module: 'MMM-PIR-Sensor',
 			config: {
 				sensorPin: 14,
@@ -103,68 +124,44 @@ var config = {
 		{
 			module: 'MMM-NetworkScanner',
 			disabled: false,
-			position: 'top_left', 
+			position: 'top_left',
 			config: {
-		        	devices: [
-		                	{ ipAddress: "<IP address>", name: "pi1", icon: "laptop"},
-			                { ipAddress: "<IP address>", name: "pi2", icon: "laptop"},
-							{ ipAddress: "<IP address>", name: "pi3", icon: "laptop"},
-							{ ipAddress: "<IP address>", name: "pi4", icon: "laptop"},
-			                { ipAddress: "<IP address>", name: "pi5", icon: "laptop"},
-			                { ipAddress: "<IP address>", name: "pi6", icon: "laptop"},
-			                ],
-		        showUnknown: false,
-		        showOffline: true,
-		        keepAlive: 120,
-		        updateInterval: 60
-		    }
-		},
-		{
-			disabled: true,
-			module: "calendar",
-			header: "Agenda",
-			position: "top_left",
-			config: {
-				calendars: [
-					{
-						symbol: "calendar-check",
-						url: "https://calendar.google.com/calendar/ical/<email>/private-<key>/basic.ics",
-						user: "",
-						pass: "",
-						method: "basic"
-					},
-					{
-						symbol: "calendar-check",
-						url: "https://calendar.google.com/calendar/ical/<email>/private-<key>/basic.ics",
-						user: "",
-						pass: "",
-						method: "basic"
-					}
-				]
+				devices: [
+					{ ipAddress: "XXXXXXXXXX", name: "NAS", icon: "laptop" },
+					{ ipAddress: "XXXXXXXXXX", name: "cuisine", icon: "laptop" },
+					{ ipAddress: "XXXXXXXXXX", name: "alarme", icon: "laptop" },
+					{ ipAddress: "XXXXXXXXXX", name: "camera portail", icon: "laptop" },
+					{ ipAddress: "XXXXXXXXXX", name: "central", icon: "laptop" },
+					{ ipAddress: "XXXXXXXXXX", name: "garage", icon: "laptop" },
+				],
+				showUnknown: false,
+				showOffline: true,
+				keepAlive: 120,
+				updateInterval: 60
 			}
 		},
 		{
-            module: 'MMM-GoogleMapsTraffic',
-            disabled: false,
-            position: 'top_left',
-            config: {
-                key: '<API key>',
-                lat: 48.8589507,
-                lng: 2.2770204,
-                height: '300px',
-                width: '300px',
-                styledMapType: "standard",
-                disableDefaultUI: true,
-                backgroundColor: 'hsla(0, 0%, 0%, 0)',
-                markers: [
-                    {
-                        lat: 48.7379665,
-                        lng: 2.0318829,
-                        fillColor: '#9966ff'
-                    },
-                ],
-            },
-        },
+			module: 'MMM-GoogleMapsTraffic',
+			disabled: true,
+			position: 'top_left',
+			config: {
+				key: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+				lat: 48.8589507,
+				lng: 2.2770204,
+				height: '300px',
+				width: '300px',
+				styledMapType: "standard",
+				disableDefaultUI: true,
+				backgroundColor: 'hsla(0, 0%, 0%, 0)',
+				markers: [
+					{
+						lat: 48.7379665,
+						lng: 2.0318829,
+						fillColor: '#9966ff'
+					},
+				],
+			},
+		},
 		{
 			module: "currentweather",
 			disabled: false,
@@ -172,7 +169,7 @@ var config = {
 			config: {
 				location: "Paris",
 				locationID: "2968815",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "<openweathermap addid>"
+				appid: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 			}
 		},
 		{
@@ -183,28 +180,7 @@ var config = {
 			config: {
 				location: "Paris",
 				locationID: "2968815",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "<openweathermap addid>"
-			}
-		},
-		{
-			module: "newsfeed",
-			disabled: false,
-			position: "bottom_bar",
-			config: {
-				feeds: [
-					{
-						title: "Le Monde",
-						url: "https://www.lemonde.fr/rss/en_continu.xml"
-					},
-					{
-                    	title: "Libération",
-                        url: "https://www.liberation.fr/rss/latest/"
-                    }
-				],
-				showSourceTitle: true,
-				showPublishDate: true,
-				broadcastNewsFeeds: true,
-				broadcastNewsUpdates: true
+				appid: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 			}
 		},
 	]
@@ -212,4 +188,4 @@ var config = {
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {module.exports = config;}
+if (typeof module !== "undefined") { module.exports = config; }
